82448b19064f5f00617ae5636597cc926c22bb97
AnderzRomero <andersonromerorico@gmail.com>
2022-09-05 14:29:12 -0500

    Personalizando Templates

--- AppRegistros/urls.py
+++ AppRegistros/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from AppRegistros.views import inicio, servicios
+from AppRegistros.views import inicio, servicios, portafolio, registros, equipo, contactos
 
 urlpatterns = [
     path('', inicio, name='AppRegistrosInicio'),
     path('servicios/', servicios, name='AppRegistrosServicios'),
+    path('portafolio/', portafolio, name='AppRegistrosPortafolio'),
+    path('registros/', registros, name='AppRegistrosRegistros'),
+    path('equipo/', equipo, name='AppRegistrosEquipo'),
+    path('contactos/', contactos, name='AppRegistrosContactos'),
 ]
--- AppRegistros/views.py
+++ AppRegistros/views.py
@@ -7,3 +7,19 @@ def inicio(request):
 def servicios(request):
 
     return render(request, 'AppBody/servicios.html')
+
+def portafolio(request):
+
+    return render(request, 'AppBody/portafolio.html')
+
+def registros(request):
+
+    return render(request, 'AppBody/registros.html')
+
+def equipo(request):
+
+    return render(request, 'AppBody/equipo.html')
+
+def contactos(request):
+
+    return render(request, 'AppBody/contactos.html')
